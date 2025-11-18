3743c800e6e12e11c858f49335a64fdc02738e44
José Luis Corral López <>
2025-11-18 16:55:12 +0100

    Exercise 5 setup

---
+++ Ejercicios acoding/Ejercicios acoding/Exercise 5 SuperHeroes/Views/SuperHeroGrid.swift
@@ -0,0 +1,18 @@
+//
+//  SuperHeroGrid.swift
+//  Ejercicios acoding
+//
+//  Created by José Luis Corral López on 18/11/25.
+//
+
+import SwiftUI
+
+struct SuperHeroGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperHeroGrid()
+}
